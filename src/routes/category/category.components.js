70bcc35e221ba1338846e36aecd6eb1015f0bf8e
salvadorf8 <aztecsalvador@gmail.com>
2022-11-22 15:12:21 -0600

    moved categories to db - categories preview - renamed some files to make sense - added nesting Route

---
+++ src/routes/category/category.components.js
@@ -0,0 +1,27 @@
+import { Fragment, useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { CategoriesContext } from '../../context/categories.context';
+import ProductCard from '../../components/product-card/product-card.component';
+
+import './category.styles.scss';
+
+const Category = () => {
+    const { category } = useParams();
+    const { categoriesMap } = useContext(CategoriesContext);
+    // const products = categoriesMap[category]; //you could use this but will render everytime instead using useState, and useEffect
+    const [products, setProducts] = useState(categoriesMap[category]);
+
+    useEffect(() => {
+        setProducts(categoriesMap[category]);
+    }, [category, categoriesMap]);
+
+    return (
+        <Fragment>
+            <h2 className='category-title'>{category.toUpperCase()}</h2>
+            <div className='category-container'>{products && products.map((product) => <ProductCard key={product.id} product={product} />)}</div>
+        </Fragment>
+    );
+};
+
+export default Category;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 // we only need one but we're going to talk about both
 import { onAuthStateChanged, getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 // after setting up Firestore DB
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import { getFirestore, doc, getDoc, setDoc, collection, query, getDocs } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -33,6 +33,37 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
 
 // after setting up Firestore DB
 export const db = getFirestore();
+
+// just a one time - below uses collection(), and writeBatch()
+// export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+//     // just searching, firestore will create automatically even if it's not there
+//     const collectionRef = collection(db, collectionKey);
+//     // here is where we need a batch - reason is to write these collection in one successful transaction
+//     const batch = writeBatch(db);
+
+//     objectsToAdd.forEach((object) => {
+//         const docRef = doc(collectionRef, object.title.toLowerCase());
+//         batch.set(docRef, object);
+//     });
+
+//     await batch.commit();
+//     console.log('done');
+// };
+
+// google constantly changes their code...uuuurrrrggggg
+export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+        const { title, items } = docSnapshot.data();
+        acc[title.toLowerCase()] = items;
+        return acc;
+    }, {});
+
+    return categoryMap;
+};
 
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
     if (!userAuth) {
