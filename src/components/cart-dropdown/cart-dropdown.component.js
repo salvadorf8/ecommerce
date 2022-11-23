aff57e135f5ac59e9c343d6bd4b6f5a6e3178d75
salvadorf8 <aztecsalvador@gmail.com>
2022-11-22 20:07:26 -0600

    styled components - NEW PATTERN with dropdown

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -6,7 +6,9 @@ import { CartContext } from '../../context/cart.context';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
-import './cart-dropdown.styles.scss';
+import { CartDropdownContainer, EmptyMessage, CartItems } from './cart-dropdown.styles';
+
+// import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
@@ -18,19 +20,19 @@ const CartDropdown = () => {
 
     // initially I used a Link, but course showed using useNavigate hook
     return (
-        <div className='cart-dropdown-container'>
-            <div className='cart-item'>
+        <CartDropdownContainer>
+            <CartItems>
                 {cartItems.length ? (
                     cartItems.map((item) => {
                         return <CartItem key={item.id} cartItem={item} />;
                     })
                 ) : (
-                    <span>Your cart is empty</span>
+                    <EmptyMessage>Your cart is empty</EmptyMessage>
                 )}
-            </div>
+            </CartItems>
 
             <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-        </div>
+        </CartDropdownContainer>
     );
 };
 
