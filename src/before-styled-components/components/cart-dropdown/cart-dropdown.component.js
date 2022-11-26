e3d747eba26bee3c5229a13d7529211be86d10ce
salvadorf8 <aztecsalvador@gmail.com>
2022-11-26 15:49:12 -0600

    NO CHANGE - organized some old resources

---
+++ src/before-styled-components/components/cart-dropdown/cart-dropdown.component.js
@@ -0,0 +1,37 @@
+import { useContext } from 'react';
+// import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+import { CartContext } from '../../context/cart.context';
+import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
+
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = () => {
+    const { cartItems } = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const goToCheckoutHandler = () => {
+        navigate('/checkout');
+    };
+
+    // initially I used a Link, but course showed using useNavigate hook
+    return (
+        <div className='cart-dropdown-container'>
+            <div className='cart-items'>
+                {cartItems.length ? (
+                    cartItems.map((item) => {
+                        return <CartItem key={item.id} cartItem={item} />;
+                    })
+                ) : (
+                    <span className='empty-message'>Your cart is empty</span>
+                )}
+            </div>
+
+            <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
+        </div>
+    );
+};
+
+export default CartDropdown;
