bdf0ae55f112b80861e8678bd115c51d402bfcee
salvadorf8 <aztecsalvador@gmail.com>
2022-12-02 15:36:24 -0600

    redux-thunk library - EXCELLENT example adding asynchronous side effects inside redux

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,6 +1,33 @@
 import { createAction } from '../../utils/reducer/reducer.utils';
 import { CATEGORIES_ACTION_TYPES } from './category.types';
 
-export const setCategories = (categoriesArray) => {
-    return createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+
+export const fetchCategoriesStart = () => {
+    return createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+};
+
+export const fetchCategoriesSuccess = (categoriesArray) => {
+    return createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesArray);
+};
+
+export const fetchCategoriesFailed = (error) => {
+    return createAction(CATEGORIES_ACTION_TYPES, error);
+};
+
+/**
+ * SF - comment - Thunk recommends that you declare your thunk actions with that last word async for readability
+ *
+ */
+export const fetchCategoriesAsync = () => async (dispatch) => {
+    dispatch(fetchCategoriesStart());
+
+    try {
+        const categoriesArray = await getCategoriesAndDocuments('categories');
+        dispatch(fetchCategoriesSuccess(categoriesArray));
+    } catch (error) {
+        dispatch(fetchCategoriesFailed(error));
+    }
+
+    return;
 };
--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import thunk from 'redux-thunk';
 
 import { rootReducer } from './root-reducer';
 
@@ -14,16 +15,21 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 /**
- *  only if environment === development render the logger
+ *  SF - comment - only if environment === development render the logger
  *  the .filter(Boolean) what this optimization does is it filters out anything that is not true
  *  (works because its an array)
  * */
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(Boolean);
-
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean);
 
 /**
- * createStore takes three parameters, third parameter is middlewares
+ * SF - comment - nice tool to use chrome extension redux tool
+ */
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+/**
+ * SF - comment - createStore takes three parameters, third parameter is middlewares
  * later we created the composedEnhancers
  * undefined = preloadedState
  */
