a609bf3b0dcc0a08922c33f65b28fd893a614c6b
salvadorf8 <aztecsalvador@gmail.com>
2023-03-07 23:32:01 -0600

    cleaned up code

--- src/store/store.js
+++ src/store/store.js
@@ -1,41 +1,30 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
-/**
- * SF - now that we have a <Spinner /> we no longer need to persist categories, the only thing we need to persist is cart
- */
+// SF - now that we have a <Spinner /> we no longer need to persist categories, the only thing we need to persist is cart
 const persistConfig = {
     key: 'root',
     storage,
     whitelist: ['cart']
 };
-
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-/**
- *  SF - comment - only if environment === development render the logger
- *  the .filter(Boolean) what this optimization does is it filters out anything that is not true
- *  (works because its an array)
- * */
+// SF - only if environment === development render the logger
+// the .filter(Boolean) what this optimization does is it filters out anything that is not true
+// (works because its an array)
 const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean);
 
-/**
- * SF - comment - nice tool to use chrome extension redux tool
- */
+// SF - nice tool to use chrome extension redux tool
 const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
-/**
- * SF - comment - createStore takes three parameters, third parameter is middlewares
- * later we created the composedEnhancers
- * undefined = preloadedState
- */
+//  SF - createStore takes three parameters, third parameter is middlewares
+//  - undefined = preloadedState
+//  - later we created the composedEnhancers
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
-
 export const persistor = persistStore(store);
